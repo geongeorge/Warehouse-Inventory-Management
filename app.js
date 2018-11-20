0996a9e5a0ca69ab65d9697531d697cfdb123315
unknown <geongeorge98@gmail.com>
2018-11-20 18:57:13 +0530

    Added admin login page

--- app.js
+++ app.js
@@ -91,6 +91,33 @@ app.post('/login', function (req, res) {
   
 })
 
+//register
+app.get('/register', function (req, res) {
+  res.render('register', {title : "Register"})
+})
+//register post
+app.post('/register', function (req, res) {
+  console.log(req.body)
+  let usr = req.body
+  let pwwd = pwHash(usr.pwd)
+  connection.query(`SELECT * from customers where username="${usr.uname}"`, function(err, rows, fields) {
+    if(rows.length <= 0){
+      //no user with specified username
+      connection.query(`INSERT into customers(username, password) values("${usr.uname}","${pwwd}")`, function(err, rows, fields) {
+        if(!err) {
+          res.redirect('/login');
+        } else {
+          res.render('register', {title : "Some error occurred", failed: true})
+        }
+      })
+
+    } else {
+      res.render('register', {title : "Username already in use", failed: true})
+    }
+  });
+
+})
+
 // Item page
 app.get('/item/:id', function (req, res) {
   res.locals.user = req.session.user
@@ -112,7 +139,7 @@ app.get('/item/:id', function (req, res) {
 })
 //buy
 app.post('/buy', function(req, res) {
-  res.send({...req.body, user:{...req.session.user}})
+  // res.send({...req.body, user:{...req.session.user}})
   let account = req.session.user
   let order = req.body
   connection.query(`insert into orders(pid,uid,qty) values("${order.product}","${account.id}",${order.qty})`, function(err, rows, fields) {
@@ -120,6 +147,7 @@ app.post('/buy', function(req, res) {
       connection.query(`UPDATE products set stock = stock - ${order.qty} where id=${order.product}`, function (error, results, fieldss) {
         if(!error)
         console.log('Successfully purchased');
+        res.redirect('/orders')
       });
     }else {
       console.log('error in purchase');
@@ -131,12 +159,26 @@ app.post('/buy', function(req, res) {
 app.get('/orders', function(req, res){
   if(req.session.user) {
     res.locals.user = req.session.user
-    connection.query(`select * from orders,products where uid = ${req.session.user.id} and orders.pid=products.id`, function(err, results, fields) {
+    connection.query(`select orders.id as oid,pid,uid,qty,status,date,name,picture from orders,products where uid = ${req.session.user.id} and orders.pid=products.id`, function(err, results, fields) {
       if(!err) {
         console.log(results)
         res.render("orders", {
           orders: results
         })
+      }
+    })
+  } else {
+    res.redirect('/404')
+  }
+})
+//confirm orders
+app.get('/orders/:id/confirm', function(req, res){
+  if(req.session.user) {
+    res.locals.user = req.session.user
+    connection.query(`update orders set status="delivered" where id = ${req.params.id}`, function(err, results, fields) {
+      if(!err) {
+        console.log(results)
+        res.redirect('/orders')
       }
     })
   } else {
@@ -156,6 +198,18 @@ app.get('/secretreg', function (req, res) {
     } 
   });
 })
+//register an Admin manually
+app.get('/secretadminreg', function (req, res) {
+  let pwwd = pwHash('password')
+  connection.query(`insert into admins(username, password) values('admin', '${pwwd}')`, function(err, rows, fields) {
+    if (!err)
+      console.log('Added User: ', rows);
+    else {
+      throw err;
+      console.log('Error while performing Query.');
+    } 
+  });
+})
 
 //log out
 app.get('/logout',(req,res)=> {
@@ -165,6 +219,41 @@ app.get('/logout',(req,res)=> {
 //log out
 app.get('/404',(req,res)=> {
   res.render('404')
+})
+
+// ****************
+//     Admin Views
+
+// ****************
+//login page
+app.get('/admin',(req,res)=> {
+  res.render("admin/login", {title: "Admin Login"})
+})
+//login script
+app.post('/admin', function (req, res) {
+  console.log(req.body)
+  let usr = req.body
+  // res.render('login', {title: "Failed! Try again", failed: true});
+
+  connection.query(`SELECT * from admins where username="${usr.uname}"`, function(err, rows, fields) {
+    if (!err) {
+      console.log('The solution is: ', rows[0]);
+
+      if(typeof rows[0] != 'undefined' && pwVerify(usr.pwd, rows[0].password)){
+        console.log("Successful Login");
+        req.session.admin = {
+          id : rows[0].id,
+          username : rows[0].username,
+        };
+        res.redirect('/');
+      } else {
+        res.render('admin/login', {title: "Failed! Try again", failed: true});
+      }
+    }
+    else
+      throw err;
+  });
+  
 })
 
 const port = process.env.PORT || 3000;
